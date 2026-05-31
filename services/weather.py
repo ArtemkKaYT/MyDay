2ec4fed8bb1ab09054ad1c0fceb04fb4c2efee5b
Artemka <korolev.artiom228@gmail.com>
2026-05-31 21:08:39 +0300

    Добавлен сервис погоды

---
+++ services/weather.py
@@ -0,0 +1,22 @@
+import requests
+from config import WEATHER_API_KEY, CITY
+
+
+WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather'
+PARAMS = {
+        'q': CITY,
+        'appid': WEATHER_API_KEY,
+        'units': 'metric',
+        'lang': 'ru'
+    }
+
+
+class WeatherService:
+    def get_weather(self):
+        try:
+            response = requests.get(WEATHER_URL, timeout=5, params=PARAMS)
+            response.raise_for_status()
+            return response.json()
+
+        except requests.exceptions.RequestException as e:
+            return e
