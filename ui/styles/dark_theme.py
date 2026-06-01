bf54e3a6ccd5bb4e78f42e0ca4c6d7ccae43d750
Artemka <korolev.artiom228@gmail.com>
2026-06-01 23:28:24 +0300

    Вынесена настройка темы приложения

---
+++ ui/styles/dark_theme.py
@@ -0,0 +1,38 @@
+DARK_THEME = """
+        QWidget {
+            background-color: #1E1E1E;
+            color: white;
+            font-size: 14px;
+        }
+
+        #timeLabel {
+            font-size: 36px;
+            font-weight: bold;
+        }
+
+        #dateLabel {
+            color: #AAAAAA;
+            font-size: 16px;
+            margin-bottom: 20px;
+        }
+
+        #briefLabel {
+            font-size: 18px;
+            padding: 15px;
+            background-color: #252526;
+            border-radius: 12px;
+            margin-bottom: 10px;
+        }
+
+        #card {
+            background-color: #252526;
+            border-radius: 15px;
+            padding: 10px;
+        }
+
+        #cardTitle {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 8px;
+        }
+    """
