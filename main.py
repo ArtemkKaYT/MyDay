cf0f36cb162db16466f6e3484a1803c25fa289b8
Artemka <korolev.artiom228@gmail.com>
2026-06-02 01:33:41 +0300

    Вынесена функция main

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+
 from ui.main_window import MainWindow
 from ui.styles.dark_theme import DARK_THEME
 
 
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
-
     app.setStyleSheet(DARK_THEME)
 
     window = MainWindow()
     window.show()
 
     sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
