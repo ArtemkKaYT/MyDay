e7acf92d266a6128af3243939ee634a54aa7fc67
Artemka <korolev.artiom228@gmail.com>
2026-06-02 23:03:13 +0300

    Исправлено отображение виджета

--- ui/widgets/notes_widget.py
+++ ui/widgets/notes_widget.py
@@ -1,9 +1,11 @@
 from PyQt6.QtWidgets import (
     QWidget,
     QVBoxLayout,
-    QListWidget,
+    QHBoxLayout,
+    QLabel,
     QPushButton,
     QLineEdit,
+    QCheckBox,
     QLayout
 )
 from PyQt6.QtCore import Qt
@@ -18,111 +20,92 @@ class NotesWidget(Card):
         notes_container = QWidget()
         notes_layout = QVBoxLayout(notes_container)
         notes_layout.setContentsMargins(0, 0, 0, 0)
-
+        notes_layout.setSpacing(10)
         notes_layout.setSizeConstraint(QLayout.SizeConstraint.SetMinAndMaxSize)
 
-        self.notes_list = QListWidget()
+        self.notes_list_container = QWidget()
+        self.notes_list_layout = QVBoxLayout(self.notes_list_container)
+        self.notes_list_layout.setContentsMargins(0, 0, 0, 0)
+        self.notes_list_layout.setSpacing(6)
 
-        self.notes_list.setVerticalScrollBarPolicy(
-            Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.notes_list.setHorizontalScrollBarPolicy(
-            Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.notes_list.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.add_note_button = QPushButton("+ Добавить заметку")
+        self.add_note_button.clicked.connect(self.toggle_note_form)
 
-        notes_layout.addWidget(self.notes_list)
-
-        self.add_note_button = QPushButton(
-            "+ Добавить заметку"
-        )
-        self.add_note_button.clicked.connect(
-            self.toggle_note_form
-        )
-
-        notes_layout.addWidget(
-            self.add_note_button
-        )
+        notes_layout.addWidget(self.notes_list_container)
+        notes_layout.addWidget(self.add_note_button)
 
         self.note_form = QWidget()
         form_layout = QVBoxLayout(self.note_form)
+        form_layout.setContentsMargins(0, 4, 0, 0)
+        form_layout.setSpacing(6)
 
         self.note_input = QLineEdit()
-        self.note_input.setPlaceholderText(
-            "Введите заметку"
-        )
+        self.note_input.setPlaceholderText("Введите заметку")
 
-        save_button = QPushButton(
-            "Сохранить заметку"
-        )
-        save_button.clicked.connect(
-            self.add_note
-        )
+        self.note_input.returnPressed.connect(self.add_note)
 
-        form_layout.addWidget(
-            self.note_input
-        )
-        form_layout.addWidget(
-            save_button
-        )
+        save_button = QPushButton("Сохранить заметку")
+        save_button.clicked.connect(self.add_note)
+
+        form_layout.addWidget(self.note_input)
+        form_layout.addWidget(save_button)
 
         self.note_form.hide()
-
-        notes_layout.addWidget(
-            self.note_form
-        )
+        notes_layout.addWidget(self.note_form)
 
         super().__init__("Заметки", body_widget=notes_container)
-
         self.load_notes()
-    
-    def toggle_note_form(self):
 
-        if self.note_form.isVisible():
-            self.note_form.hide()
-        else:
-            self.note_form.show()
-        
-        self.adjustSize()
-        if self.window():
-            self.window().adjustSize()
+    def toggle_note_form(self):
+        self.note_form.setVisible(not self.note_form.isVisible())
+
+    def clear_notes_layout(self):
+        while self.notes_list_layout.count():
+            item = self.notes_list_layout.takeAt(0)
+            widget = item.widget()
+            if widget:
+                widget.deleteLater()
 
     def load_notes(self):
-
-        self.notes_list.clear()
-
+        self.clear_notes_layout()
         notes = self.notes_service.get_notes()
 
-        for note in notes:
-            self.notes_list.addItem(
-                note["text"]
-            )
-        
-        self.update_list_height()
-    
-    def update_list_height(self):
-
-        if self.notes_list.count() == 0:
-            self.notes_list.setFixedHeight(0)
+        if not notes:
+            no_notes_label = QLabel("Заметок пока нет")
+            no_notes_label.setObjectName("noNotesLabel")
+            self.notes_list_layout.addWidget(no_notes_label)
             return
 
-        total_height = 0
-        for i in range(self.notes_list.count()):
-            total_height += self.notes_list.sizeHintForRow(i)
+        for note in notes:
+            row_widget = QWidget()
+            row_widget.setObjectName("noteRow")
+            row_layout = QHBoxLayout(row_widget)
+            row_layout.setContentsMargins(10, 8, 10, 8)
+            row_layout.setSpacing(10)
 
-        total_height += self.notes_list.frameWidth() * 2 + 12
+            checkbox = QCheckBox()
+            checkbox.setObjectName("noteCheckBox")
+            checkbox.clicked.connect(lambda _, note_id=note["id"]: self.delete_note(note_id))
 
-        self.notes_list.setFixedHeight(total_height)
-        
-        self.adjustSize()
-        if self.window():
-            self.window().adjustSize()
+            text_label = QLabel(note["text"])
+            text_label.setObjectName("noteText")
+            text_label.setWordWrap(True)
+
+            row_layout.addWidget(checkbox, alignment=Qt.AlignmentFlag.AlignVCenter)
+            row_layout.addWidget(text_label, stretch=1, alignment=Qt.AlignmentFlag.AlignVCenter)
+
+            self.notes_list_layout.addWidget(row_widget)
 
     def add_note(self):
+        text = self.note_input.text().strip()
+        if not text:
+            return
 
-            text = self.note_input.text().strip()
-            if not text:
-                return
+        self.notes_service.add_note(text)
+        self.load_notes()
+        self.note_input.clear()
+        self.note_form.hide()
 
-            self.notes_service.add_note(text)
-            self.load_notes()
-            self.note_input.clear()
-            self.note_form.hide()
+    def delete_note(self, note_id):
+        self.notes_service.delete_note(note_id)
+        self.load_notes()
