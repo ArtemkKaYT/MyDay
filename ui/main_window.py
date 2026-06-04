db9806a1e9ceeabe2d5ad9102210856502a2710b
ivanzhdakhin <trollgameshoy@gmail.com>
2026-06-04 19:31:32 +0300

    Добавлена документация

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,3 +1,7 @@
+"""
+@file theme_meneger.py
+@brief Module of MyDay project.
+"""
 from PyQt6.QtCore import QTimer, QPropertyAnimation, QThread, pyqtSignal
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QScrollArea
 
